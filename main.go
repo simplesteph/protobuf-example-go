6108c701aac21d36686be1abeb7d3d3d39a0395b
Stephane Maarek <simplesteph@users.noreply.github.com>
2018-06-09 17:45:48 +0800

    added to / from json code

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 
+	"github.com/golang/protobuf/jsonpb"
 	"github.com/golang/protobuf/proto"
 	"github.com/simplesteph/protobuf-example-go/src/simple"
 )
@@ -13,6 +14,33 @@ func main() {
 	sm := doSimple()
 
 	readAndWriteDemo(sm)
+	jsonDemo(sm)
+}
+
+func jsonDemo(sm proto.Message) {
+	smAsString := toJSON(sm)
+	fmt.Println(smAsString)
+
+	sm2 := &simplepb.SimpleMessage{}
+	fromJSON(smAsString, sm2)
+	fmt.Println("Successfully created proto struct:", sm2)
+}
+
+func toJSON(pb proto.Message) string {
+	marshaler := jsonpb.Marshaler{}
+	out, err := marshaler.MarshalToString(pb)
+	if err != nil {
+		log.Fatalln("Can't convert to JSON", err)
+		return ""
+	}
+	return out
+}
+
+func fromJSON(in string, pb proto.Message) {
+	err := jsonpb.UnmarshalString(in, pb)
+	if err != nil {
+		log.Fatalln("Couldn't unmarshal the JSON into the pb struct", err)
+	}
 }
 
 func readAndWriteDemo(sm proto.Message) {
